13cfef97d8ba1bd95cc26b29a2ea1d7b2162a1d7
Arsharaj Chauhan <arsharajchauhan343@gmail.com>
2020-09-16 16:07:57 +0530

    updated c++ template

--- cpp/code.cpp
+++ cpp/code.cpp
@@ -8,18 +8,38 @@
 #include <algorithm> // for stl algorithms
 #include <numeric>   // for numeric operations
 #include <cctype>    // for character handling
+#include <cmath>     // for mathematical operations
 #include <string>    // for string operations
 #include <new>       // for dynamic memory
 #include <limits>    // for arithematic limits
 // Namespaces
 using namespace std; 
 // Function Definitions
+void setup_start();                   // competitive programming setup start
+void setup_end();                   // competitive programming setup end
 void solution(void);            // solution for each test case
 /************
 input: void
 output: integer
 *************/
 int main(void)
+{
+    setup_start();
+    int t;
+    cin >> t;
+    while(t)
+    {
+        solution();
+        t--;
+    }
+    setup_end();
+    return 0;
+}
+/************
+input: void 
+output: void
+************/
+void setup_start(void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); 
@@ -28,16 +48,15 @@ int main(void)
         freopen("output.txt", "w", stdout); 
         freopen("error.txt", "w", stderr); 
     #endif 
-    int t;
-    cin >> t;
-    while(t)
-    {
-        solution();
-        t--;
-    }
+}
+/************
+input: void 
+output: void
+************/
+void setup_end()
+{
     float run_time = (float)clock() / CLOCKS_PER_SEC;
     cerr << "Time taken : " << run_time << " secs" << endl;
-    return 0;
 }
 /************
 input: void
