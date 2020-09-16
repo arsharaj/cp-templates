8959894b5eacfd26a719470d159946602f2089a0
Arsharaj Chauhan <arsharajchauhan343@gmail.com>
2020-09-16 14:42:02 +0530

    Add the python template

---
+++ py/code.py
@@ -0,0 +1,22 @@
+# Author - Arsharaj Chauhan
+import os       # for operating system functionalities
+import sys      # for system functionalities
+import math     # for mathematical operations and functions
+import random   # for random number generation
+
+def solution():
+    '''
+    input: none, no parameter is provided
+    output: none, no parameter is provided
+    '''
+    pass
+
+if __name__ == "__main__":
+    '''
+    input: none, no input to the function
+    output: integer, exit status code
+    '''
+    c = int(input())
+    for i in range(c):
+        solution()
+    sys.exit(0)
