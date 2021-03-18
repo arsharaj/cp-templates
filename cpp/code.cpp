0b01b60bbfd9ba16d697f6a3b3588a7af78998d5
Arsharaj Chauhan <arsharajchauhan343@gmail.com>
2021-03-19 01:34:13 +0530

    Updated template revision 1

--- cpp/code.cpp
+++ cpp/code.cpp
@@ -1,68 +1,14 @@
-// Author - Arsharaj Chauhan
-#include <iostream>  // for inputs and outputs
-#include <vector>    // for dynamic arrays and pairs
-#include <stack>     // for lifo
-#include <queue>     // for fifo and priority queue
-#include <array>     // for same and fixed data
-#include <iterator>  // for pointers
-#include <algorithm> // for stl algorithms
-#include <numeric>   // for numeric operations
-#include <cctype>    // for character handling
-#include <cmath>     // for mathematical operations
-#include <string>    // for string operations
-#include <new>       // for dynamic memory
-#include <limits>    // for arithematic limits
-// Namespaces
-using namespace std; 
-// Function Definitions
-void setup_start();                   // competitive programming setup start
-void setup_end();                   // competitive programming setup end
-void solution(void);            // solution for each test case
-/************
-input: void
-output: integer
-*************/
-int main(void)
-{
-    setup_start();
-    int t;
-    cin >> t;
-    while(t)
-    {
-        solution();
-        t--;
-    }
-    setup_end();
+#include <bits/stdc++.h>
+#define REP(i, a, b) for(int i = a; i <= b; i++)
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    // Solution
     return 0;
-}
-/************
-input: void 
-output: void
-************/
-void setup_start(void)
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); 
-    #ifndef FILES 
-        freopen("input.txt", "r", stdin); 
-        freopen("output.txt", "w", stdout); 
-        freopen("error.txt", "w", stderr); 
-    #endif 
-}
-/************
-input: void 
-output: void
-************/
-void setup_end()
-{
-    float run_time = (float)clock() / CLOCKS_PER_SEC;
-    cerr << "Time taken : " << run_time << " secs" << endl;
-}
-/************
-input: void
-output: void
-*************/
-void solution(void)
-{
-    
 }
